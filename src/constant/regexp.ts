4be4a41f8b7f06126fd87430ae4569c4ae3cc297
RequireSun <kelvinsen@outlook.com>
2019-05-31 15:44:19 +0800

    chore: auto fix

--- src/constant/regexp.ts
+++ src/constant/regexp.ts
@@ -1,4 +1,3 @@
-
 // {用户 ID} {预订日期 yyyy-MM-dd} {预订时间段 HH:mm~HH:mm} {场地}
 // {用户 ID} {预订日期 yyyy-MM-dd} {预订时间段 HH:mm~HH:mm} {场地} {取消标记}
 export const regInput = /^(\w+)\s(\d{4}-\d{1,2}-\d{1,2})\s(\d{1,2}:\d{2})~(\d{1,2}:\d{2})\s(\w+)(?:\s(\w))?$/;
--- src/judgement/isFree.ts
+++ src/judgement/isFree.ts
@@ -1,4 +1,3 @@
-
 export default function judgeIsFree(sameBooked: IBooking[], startTime: number, endTime: number): boolean {
     for (let i = 0, l = sameBooked.length; i < l; ++i) {
         // 当前的时间比已预定的结束时间早, 或者结束比已预订的晚, 证明重合了
